09d847547fa4050c8dd03bd5217b0554ca6cdc54
famaterialsg <fa.material.sg@gmail.com>
2024-09-30 12:57:48 +0700

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     
     # Authentication URLs
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
 
